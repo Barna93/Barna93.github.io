37bf83a13aef9b3c08fca9fb35347c79390def01
Joe <boszebarna@citromail.hu>
2022-06-21 15:14:39 +0200

    asd

---
+++ AdminJs/admin.js
@@ -0,0 +1,37 @@
+// Get server data
+function getServerData (url) {
+    let fetchOptions ={
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache"
+    };
+    return fetch(url,fetchOptions).then(
+        response=>response.json(),
+        err =>console.error(err)
+       
+        );
+}
+
+document.querySelector("#getDataBtn").addEventListener("click",function(){
+    getServerData("http://localhost:3000/users") .then(
+        data =fillDataTable(data,"userTable")
+        );
+
+});
+
+// fill table with server data.
+function fillDataTable(data, tableID){
+    let table= document.querySelector(`#${tableID}`);
+    if(!table){
+        console.error(`Table"${tableID}"is not found`);
+        return;
+    }
+    let tBody =table.querySelector("tbody");
+    for(let row of data){
+        console.log(row);
+    }
+}
+
+
+
+
